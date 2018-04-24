021d1f2a8d14a31d7bc12f4a333d5cf3f2cd04d5
Ptombak <kuaybece@gmail.com>
2018-04-24 19:16:31 -0400

    first commit

--- src/demo/integretion.java
+++ src/demo/integretion.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,8 +17,8 @@ public class integretion {
 	@BeforeTest
 	public void setUp() {
 		
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\CybertekStudent\\Downloads\\chromedriver_win32.zip");
-
+		System.setProperty("webdriver.chrome.driver","C:\\Selenium_Lib\\chromedriver.exe");
+        driver=new ChromeDriver();
 		driver.get("http://www.google.com");
 		
 		
---
+++ test_runner2.bat
@@ -0,0 +1,1 @@
+java -cp C:\Users\CybertekStudent\workspace\jenkins_github_Demo\bin;C:\Selenium_Lib\*;C:\Users\CybertekStudent\.p2\pool\plugins\* org.testng.TestNG testng.xml   
