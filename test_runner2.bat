740d8153ed83c2701d78e30efe3791e5c5d8b5da
Ptombak <kuaybece@gmail.com>
2018-04-24 19:28:58 -0400

    mytest

--- test_runner2.bat
+++ test_runner2.bat
@@ -1,1 +1,1 @@
-java -cp C:\Users\CybertekStudent\workspace\jenkins_github_Demo\bin;C:\Selenium_Lib\*;C:\Users\CybertekStudent\.p2\pool\plugins\* org.testng.TestNG testng.xml   
+java -cp C:\Users\CybertekStudent\workspace\demo\bin;C:\Selenium_Lib\*;C:\Users\CybertekStudent\.p2\pool\plugins\* org.testng.TestNG testng.xml   
